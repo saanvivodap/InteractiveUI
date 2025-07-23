91efe0722d3b95b651f3d8b32f1521e739d01905
Saanvi <saanvivodapally@gmail.com>
2025-07-23 13:44:55 -0700

    Added Initial Text

--- InteractiveUI/ContentView.swift
+++ InteractiveUI/ContentView.swift
@@ -10,10 +10,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("What is Your Name?")
+                .font(.title)
+                .fontWeight(.bold)
+                .foregroundColor(Color.blue)
         }
         .padding()
     }
